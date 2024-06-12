f98290b8a5480fa34d96d896e660bc8b42656ee2
Saad-Y <swy03@mail.aub.edu>
2024-06-12 12:58:47 +0300

    combined utils and extensions in one file

--- extensions.py
+++ extensions.py
@@ -8,12 +8,19 @@ import jwt, re
 import os
 
 
+import numpy as np
+from openai import OpenAI
+
+
 SECRET_KEY= os.getenv('SECRET_KEY')
+client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+
 
 db = SQLAlchemy()
 ma = Marshmallow()
 bcrypt = Bcrypt()
 migrate = Migrate()  
+
 
 
 def create_token(user_id):
@@ -127,3 +134,30 @@ def validate_password(password):
         number_pattern.search(password) is not None and
         special_symbol_pattern.search(password) is not None
     )
+
+
+
+def get_embeddings(text):
+    response = client.embeddings.create(
+        model="text-embedding-ada-002",
+        input=text,
+        encoding_format="float"
+    )
+    return response.data[0].embedding 
+
+def cosine_similarity(vec1, vec2):
+    return np.dot(vec1, vec2) / (np.linalg.norm(vec1) * np.linalg.norm(vec2))
+
+def select_relevant_few_shots(user_question, few_shot_examples, top_n=3):
+    user_embedding = get_embeddings(user_question)
+    similarities = []
+
+    for example in few_shot_examples:
+        example_embedding = get_embeddings(example.question)
+        similarity = cosine_similarity(user_embedding, example_embedding)
+        similarities.append((example, similarity))
+
+    # Sort examples by similarity and select top_n
+    similarities.sort(key=lambda x: x[1], reverse=True)
+    relevant_examples = [ex[0] for ex in similarities[:top_n]]
+    return relevant_examples
